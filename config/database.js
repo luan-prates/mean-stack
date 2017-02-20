82967261f7181a3d6c1991ba367d62497817fd76
luan-prates <pratesr21@gmail.com>
2017-02-20 14:42:31 -0300

    tradução das mensagens de erro de validação de forma global.

--- config/database.js
+++ config/database.js
@@ -2,3 +2,6 @@ const mongoose = require('mongoose');
 module.exports = mongoose.connect('mongodb://localhost/db_finance');
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
+mongoose.Error.messages.Number.min = "O valor '{VALUE}' é menor que o limite mínimo de '{MIN}'.";
+mongoose.Error.messages.Number.max = "O valor '{VALUE}' é maior que o limite máximo de '{MAX}'.";
+mongoose.Error.messages.toString.enum = "'{VALUE} não é válido para o atributo '{PATH}'.";
