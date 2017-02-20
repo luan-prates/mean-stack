f4f6b58263a48d720aab2b3413ef54afd0944a53
luan-prates <pratesr21@gmail.com>
2017-02-20 14:29:05 -0300

    retornar um obj novo depois do update.
    billingCycle.updateOptions({ new: true });
    
    essa configuração faz o objeto atualizado ser retornado.

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,5 +1,6 @@
 const billingCycle = require('./billingCycle');
 
 billingCycle.methods(['get', 'post', 'put', 'delete']);
+billingCycle.updateOptions({ new: true });
 
 module.exports = billingCycle;
