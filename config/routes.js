3e3082e19b4e406fd7e0e4f599ab6d9e338f8f56
luan-prates <pratesr21@gmail.com>
2017-02-20 13:52:22 -0300

    rotas da API
    Instânciei uma constante billingCycleService e usei o metodo register
    passando o router e o caminho '/billingCycles'

--- config/routes.js
+++ config/routes.js
@@ -4,5 +4,10 @@ module.exports = function(server){
 
     //API Routes
     const router = express.Router();
-    server.use('/api', router)
+    server.use('/api', router);
+
+    //rotas da API
+    const billingCycleService = require('../api/billingCycle/billingCycleService');
+    billingCycleService.register(router, '/billingCycles');
+
 }
