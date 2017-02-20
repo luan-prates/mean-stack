7eee1b0ca6efca04181ccef80cc4a121bee80023
luan-prates <pratesr21@gmail.com>
2017-02-20 14:33:12 -0300

    validando as atualizações

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,6 +1,6 @@
 const billingCycle = require('./billingCycle');
 
 billingCycle.methods(['get', 'post', 'put', 'delete']);
-billingCycle.updateOptions({ new: true });
+billingCycle.updateOptions({ new: true, runValidators: true });
 
 module.exports = billingCycle;
