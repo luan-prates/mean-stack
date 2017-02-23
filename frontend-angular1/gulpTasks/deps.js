ff6156074708c08aaf96a0323fd7ddd78264badf
luan-prates <pratesr21@gmail.com>
2017-02-23 17:09:42 -0300

    configurada a estrutrura da bild da aplicação.
    usando o gulp estruturamos a estutura que deve ser executada caso a
    aplicação deja chamada como dev ou production.

---
+++ frontend-angular1/gulpTasks/deps.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+
+gulp.task('deps', ['deps.css', 'deps.js', 'deps.fonts']);
+
+gulp.task('deps.css', function () {
+    
+});
+
+gulp.task('deps.js', function () {
+    
+});
+
+gulp.task('deps.fonts', function () {
+    
+});
