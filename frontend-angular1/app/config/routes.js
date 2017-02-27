6c556be28848692ebf155dcccb1a673dac1fd14f
luan-prates <pratesr21@gmail.com>
2017-02-27 17:29:14 -0300

    configurando as rotas

---
+++ frontend-angular1/app/config/routes.js
@@ -0,0 +1,18 @@
+/**
+ * Created by tilua on 27/02/2017.
+ */
+angular.module('primeiraApp').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('dashboard', {
+            url:"/dashboard",
+            templateUrl: "dashboard/dashboard.html"
+        }).state('billingCycle', {
+            url:"/billingCycles",
+            templateUrl: "billingCycle/tabs.html"
+        })
+
+        $urlRouterProvider.otherwise('/dashboard')
+    }
+]);
