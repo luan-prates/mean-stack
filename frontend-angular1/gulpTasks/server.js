395eac5e5e3847599cd7632daa746d8202417a51
luan-prates <pratesr21@gmail.com>
2017-02-27 16:20:48 -0300

    correção do erro de importação dos templates.

--- frontend-angular1/gulpTasks/server.js
+++ frontend-angular1/gulpTasks/server.js
@@ -5,14 +5,14 @@ const webserver = require('gulp-webserver')
 gulp.task('server', ['watch'], function() {
   gulp.src('public').pipe(webserver({
     livereload: true,
-    port: 4000,
+    port: 3000,
     open: true
   }))
 })
 
 gulp.task('watch', function() {
-  watch('app/**/*.html', () => gulp.start('app.html'))
-  watch('app/**/*.css', () => gulp.start('app.css'))
-  watch('app/**/*.js', () => gulp.start('app.js'))
-  watch('assets/**/*.*', () => gulp.start('app.assets'))
+  watch('app/**/*.html', function() { gulp.start('app.html')})
+  watch('app/**/*.css', function() { gulp.start('app.css')})
+  watch('app/**/*.js', function() { gulp.start('app.js')})
+  watch('assets/**/*.*', function() { gulp.start('app.assets')})
 })
