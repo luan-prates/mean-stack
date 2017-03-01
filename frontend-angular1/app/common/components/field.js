508c31723701ee7ede71381514218a8af752334f
luan-prates <pratesr21@gmail.com>
2017-03-01 16:00:38 -0300

    criando o contrller para os ciclos de pagamento

--- frontend-angular1/app/common/components/field.js
+++ frontend-angular1/app/common/components/field.js
@@ -6,11 +6,12 @@
             grid: '@',
             placeholder: '@',
             type: '@',
+            model: '=',
         },
         controller:[
             'gridSystem',
             function (gridSystem) {
-                this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid);
+                this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
             }
         ],
         template:`
@@ -18,7 +19,8 @@
                 <div class="form-group">
                     <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
                     <input id="{{ $ctrl.id }}" class="form-control" 
-                    placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}"/>
+                    placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}"
+                    ng-model="$ctrl.model"/>
                 </div>
             </div>
         `
