1c83aec3930b5f12ee370ffd9a7eb8ff2e822993
luan-prates <pratesr21@gmail.com>
2017-03-02 09:43:14 -0300

    Criação da função refresh
    criamos a função refresh para fazer uma nova requisição, zerar o obj
    billingCycle existente e adicionar em um novo obj os valores retornados
    do backend

--- frontend-angular1/app/billingCycle/billingCycleController.js
+++ frontend-angular1/app/billingCycle/billingCycleController.js
@@ -2,18 +2,30 @@
    angular.module('primeiraApp')
        .controller('BillingCycleCtrl', [
        '$http',
+       'msgs',
        BillingCycleController
    ])
     
-    function BillingCycleController($http) {
+    function BillingCycleController($http, msgs) {
         const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
 
-        vm.create = function () {
-            const url = 'http://localhost:3003/api/billingCycles'
-            $http.post(url, vm.billingCycle).then(function (response) {
+        vm.refresh = function () {
+            $http.get(url).then(function (res) {
                 vm.billingCycle = {}
-
+                vm.billingCycles = res
+            })    
+        }
+        
+        vm.create = function () {
+            $http.post(url, vm.billingCycle).then(function(res) {
+                vm.refresh();
+                msgs.addSuccess('Operação realizada com sucesso!');
+            }).catch(function(res){
+                msgs.addError(res.data.errors);
             })
         }
+
+        vm.refresh();
     }
 })()
